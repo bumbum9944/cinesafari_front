61d2daa37e601c0ba96a19b751cc98ab2f72da28
vxda7 <vxda77@gmail.com>
2019-11-22 12:05:30 +0900

    margin

--- src/components/BoxOffice.vue
+++ src/components/BoxOffice.vue
@@ -1,11 +1,13 @@
 <template>
   <div class="boxoffice mx-3">
-    <h2 class="mb-4">BoxOffice</h2>
+    <h2 class="mx-2 mb-2">BoxOffice</h2>
     <div class="row">
-      <div v-for="(movie, idx) in boxOfficeList" :key="idx" class="card col-2 m-2">
-        <img :src="movie.image" class="card-img-top" alt="">
-        <div class="card-body">
-          <h5 class="card-title">{{movie.movieNm}}</h5>
+      <div v-for="(movie, idx) in boxOfficeList" :key="idx" class="col-2 mx-2 p-0">
+        <div class="card m-2">
+          <img :src="movie.image" class="card-img-top" alt="">
+          <div class="card-body">
+            <h5 class="card-title">{{movie.movieNm}}</h5>
+          </div>
         </div>
       </div>
     </div>
